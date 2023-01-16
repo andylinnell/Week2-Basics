3ca3df2cb7fef07692077a06ff60504697f37b4e
Andy Linnell <andylinnell.cafo@gmail.com>
2023-01-16 15:41:23 -0500

    Quiz on material from week 1

---
+++ quiz.js
@@ -0,0 +1,18 @@
+const person = {
+    fullName:"Andy Linnell",
+    age: 30,
+    confident: true
+
+}
+
+function addNums (x,y){
+    let sum = x + y;
+    return sum;
+}
+console.log(addNums(3,2));
+
+
+for(let i = 1 ; i <= 10; i++){
+    console.log(i);
+}
+
